4d6733d7c49ef04d36b1087793a5c4ebc12b31a9
xiaoniul <1599545452@qq.com>
2019-12-11 19:53:50 +0800

    Merge branch 'master' of https://github.com/xiaoniul/erp_project
    # Conflicts:
    #	src/pages/login/login.html

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,3 +1,12 @@
 /**
  * Created by 86130 on 2019/11/23.
  */
+
+import Vue from 'vue'
+import Index from './index.vue'
+
+new Vue({
+  el: '#app',
+  components: { Index },
+  template: '<Index/>'
+})
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,3 +1,13 @@
 /**
  * Created by 86130 on 2019/11/23.
  */
+import Vue from 'vue'
+import Login from './login.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  components: { Login },
+  template: '<Login/>'
+})
